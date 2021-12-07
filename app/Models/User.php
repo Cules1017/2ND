d2c9803af0fd2d695ac517e51aa5cd4346f74801
Vainres <64851127+Vainres@users.noreply.github.com>
2021-12-07 21:47:46 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'email',
         'phone', 
         'password',
+        'provider' => $provider,
+        'provider_id' => $user->id,
     ];
 
     /**
